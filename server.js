75420d46e0dce0346d4f7fdb0db68cf874c99a93
yukariIm8 <55606330+miirakuy@users.noreply.github.com>
2020-07-17 21:40:30 -0700

    feat: created environment settings, POST and GET routes.

--- server.js
+++ server.js
@@ -2,18 +2,49 @@
 projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+const port = 8000;
+const server = app.listen(port, listening);
+
+function listening() {
+    console.log(`Server is running on local host: ${port}`);
+};
+
+// POST route
+app.post('/add', postData);
+
+function postData(request, response) {
+    projectData = {
+        temperature: request.body.temperature,
+        date: request.body.date,
+        feelings: request.body.feelings,
+    };
+    response.send(projectData);
+    console.log(projectData);
+};
+
+// GET route
+app.get('/all', getData);
+
+function getData(request, response) {
+    response.send(projectData);
+};
