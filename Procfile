4116d5549db7a90aca087e369b7448b2c63d58b0
yukariIm8 <55606330+miirakuy@users.noreply.github.com>
2020-08-27 00:42:55 -0700

    chore: modify config for heroku.

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node server.js
+web: node server.jsheroku logs --tail
--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(express.static('website'));
 
 // Setup Server
 const port = 8000;
-const server = app.listen(port, listening);
+const server = app.listen(process.env.PORT || port, listening);
 
 function listening() {
     console.log(`Server is running on local host: ${port}`);
